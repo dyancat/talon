54cbb48b7360ac70fba0a4ef59283a231abd2aaa
David Yan <davidyan6@gmail.com>
2022-12-04 11:17:43 +1300

    Keys

--- code/keys.py
+++ code/keys.py
@@ -1,6 +1,6 @@
 from talon import Context, Module, actions, app
 
-default_alphabet = "air bite cap dan each fine gust house sit jam crunch look made name odd pink queen red sun trap you vacant week plex yank zinc".split(
+default_alphabet = "air bat cap dan each fine gust house sit jam crunch look made name odd pink queen red sun trap you vacant week plex yank zinc".split(
     " "
 )
 letters_string = "abcdefghijklmnopqrstuvwxyz"
@@ -248,6 +248,6 @@ special_keys = {k: k for k in simple_keys}
 special_keys.update(alternate_keys)
 ctx.lists["self.special_key"] = special_keys
 ctx.lists["self.function_key"] = {
-    f"function {default_f_digits[i]}": f"f{i + 1}" for i in range(12)
+    f"funk {default_f_digits[i]}": f"f{i + 1}" for i in range(12)
 }
 
